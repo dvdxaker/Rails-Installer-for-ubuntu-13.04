70de011ca1a18913640e93f5c326f5905384c357
dvdxaker <ruscreative@yandex.ru>
2013-07-05 13:12:37 -0700

    Update Install_rails.sh
    Update + Upgrade OS

--- Install_rails.sh
+++ Install_rails.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-# A simple Script for installing Rails on Ubuntu 13.04
+# A simple Script for installing Rails on Ubuntu 13.04(for CLEAR SYSTEM!!!)
+# Author: DVD_Xaker (ruscreative@ya.ru)
+# License: MIT
+
+RUBY_VERSION="1.9.3"
+LOG_FILE="$HOME/rails_install.log"
+echo "#######################################"
+echo "######## Ruby + Rail installer ########"
+echo "#######################################"
+
+# Update OS Ubuntu 13.04
+echo -n "Updating package cache..."
+sudo apt-get update >>$LOG_FILE
+echo "Done"
+# Upgrade OS Ubuntu 13.04
+echo -n "Upgrading package cache..."
+sudo apt-get upgrade >>$LOG_FILE
+echo "Done"
